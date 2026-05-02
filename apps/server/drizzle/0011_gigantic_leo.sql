bbce45844cb94d9ad9de95b1b7296d334bd656a9
Samuel Deng <dengzhaofun@gmail.com>
2026-05-02 18:50:22 +0800

    fixup: drop local 0010/0011 migrations to rebase on main

--- apps/server/drizzle/0011_gigantic_leo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "experiment_experiments" ADD COLUMN "primary_metric" jsonb;--> statement-breakpoint
-ALTER TABLE "experiment_experiments" ADD COLUMN "metric_window_days" integer DEFAULT 7 NOT NULL;
